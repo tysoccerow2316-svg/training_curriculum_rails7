7c484969e18b049b00bb0df3b2c5f617d1cf3791
tysoccerow2316-svg <ty.soccer.ow.2316@gmail.com>
2026-02-24 19:21:01 +0900

    issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -29,20 +29,13 @@ class CalendarsController < ApplicationController
 
     @week_days = []
 
-    plans = Plan.where(date: @todays_date..@todays_date + 6)
     plans_for_week = Plan.where(date: @todays_date..@todays_date + 6).group_by(&:date)
 
-    
-
     7.times do |x|
-      today_plans = []
-      plans.each do |plan|
-        today_plans.push(plan.plan) if plan.date == @todays_date + x
-      end
-
-      days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday => wdays[(@todays_date.wday + x) % 7]}
+      current_date = @todays_date + x
+      plans_for_the_day = plans_for_week[current_date] || []
+      days = { month: current_date.month, date: current_date.day, plans: plans_for_the_day.map(&:plan), wday: wdays[current_date.wday] }
       @week_days.push(days)
-
     end
   end
 end
